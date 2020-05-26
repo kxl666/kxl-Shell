dc575465da4789c8c47e8d63a94270e441cae8f2
kxl666 <64139784+kxl666@users.noreply.github.com>
2020-05-26 09:54:37 +0800

    Delete login.sh..sh

--- login.sh..sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-y=` w -i |grep 'pts'|grep 'root' | awk '{print $4}'| awk 'BEGIN{FS=":"}{print $1}'`
-x=`date | awk '{print $5}' | awk -F":" '{print $1}'`
-if [ $y -eq $x ]
-   then
-    echo "欢迎飞哥登录" | mail -s '大佬柯全飞'  2244951869@qq.com
-fi
