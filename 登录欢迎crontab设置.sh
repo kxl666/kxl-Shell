3af382dc8f430ec13bc7429a635cca5925fc3d1b
kxl666 <64139784+kxl666@users.noreply.github.com>
2020-07-04 09:51:40 +0800

    Add files via upload

---
+++ 登录欢迎crontab设置.sh
@@ -0,0 +1,14 @@
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+
+#* * * * * sleep 10;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 15;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 20;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 25;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 30;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 35;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 40;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 45;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 50;  . /etc/profile; /bin/sh /tmp/login.sh
+#* * * * * sleep 55;  . /etc/profile; /bin/sh /tmp/login.sh
