bfd3321471a770f951f1f6135bdb29f78badb0e7
柯小乐 <64139784+kxl666@users.noreply.github.com>
2020-10-26 22:54:23 +0800

    Update 杰科-2000图片流.sh

--- 杰科-2000图片流.sh
+++ 杰科-2000图片流.sh
@@ -22,11 +22,20 @@ echo "{
 \"limit\": 1
 }
 " >10.txt
-if curl --header "session_id: 127096891@wuhanshizhedt_1563622360" -d '@10.txt'  http://100.119.30.20:11180/business/api/condition/query_camera |grep "OK" &>/dev/null ;then echo $line >> ipzz.txt;fi
+
+#最终比对合适的摄像头id
+if curl --header "session_id: 127096891@wuhanshizhedt_1563622360" -d '@10.txt'  http://100.119.30.20:11180/business/api/condition/query_camera |grep "OK" &>/dev/null 
+then
+ if ! grep $line {21这台机子的摄像头的id列表文件}
+ then
+ echo $line >>ipzz.txt
+ fi
+fi
+#echo $line >> ipzz.txt;fi
 #sed -rn '/camera_ids/s#(.*\[")(.*)#\1$line"\]#'p 10.txt >kxl.txt
 #sed -rn "/camera_ids/c "camera_ids": ["\$line"]"p 10.txt >kxl.txt
 done < <(cat 7.txt | egrep -o "[0-9]{1,}")
 
-wait 
-#最终比对合适的摄像头id
-diff -c {21这台机子的摄像头的id列表文件} ipzz.txt | egrep '^[-!] [0-9]{1,}' | egrep -o "[0-9]{1,}" >合适摄像头id文件
+#wait 
+#最终比对合适的摄像头id(筛选有小问题需优化)
+#diff -c {21这台机子的摄像头的id列表文件} ipzz.txt | egrep '^[-!] [0-9]{1,}' | egrep -o "[0-9]{1,}" >合适摄像头id文件
