3f3e1c94bf3953950900be7ba9a5d3e356823200
柯小乐 <2244951869@qq.com>
2021-01-14 09:57:31 +0800

    Update and rename 注释多行.sh to 注释多行+一些即兴知识点.sh

---
+++ 注释多行+一些即兴知识点.sh
@@ -0,0 +1,11 @@
+ls 
+<<EOF
+echo "line 1"
+echo "line 2"
+echo "line 3"
+EOF
+
+# vim .bash_profile
+# export PATH=$PATH:/usr/local/python37/bin
+# 源码安装 定义路径 
+# source .bash_profile
--- 注释多行.sh
+++
@@ -1,6 +0,0 @@
-ls 
-<<EOF
-echo "line 1"
-echo "line 2"
-echo "line 3"
-EOF
