8c4c9b53fa5884f83719bfda0a2cfd293471c891
柯小乐 <2244951869@qq.com>
2021-01-18 22:34:09 +0800

    Update 注释多行+一些即兴知识点.sh

--- 注释多行+一些即兴知识点.sh
+++ 注释多行+一些即兴知识点.sh
@@ -10,6 +10,9 @@ EOF
 # 源码安装 定义路径 
 # source .bash_profile
 
-# 修改PSI
+# 修改PSI .bashrc
 export PS1="\[\033]2;\h:\u \w\007\033[32;1m\]\u@$ip\h \033[35;1m\t\033[0m \[\033[34;1m\]\w\[\033[0m\] \[\e[31;1m\] # \[\e
 [0m\]"
+# 修改全局PSI /etc/bashrc
+[ "$PS1" = "\\s-\\v\\\$ " ] && PS1='\[\e[34m\][\u@\h\t \W]#\[\e[m\] '
+
