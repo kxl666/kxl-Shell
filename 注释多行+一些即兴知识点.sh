8046114327e738a4ea39b7f8a01da9f4245bcf7e
柯小乐 <2244951869@qq.com>
2021-01-18 22:28:54 +0800

    Update 注释多行+一些即兴知识点.sh

--- 注释多行+一些即兴知识点.sh
+++ 注释多行+一些即兴知识点.sh
@@ -9,3 +9,7 @@ EOF
 # export PATH=$PATH:/usr/local/python37/bin
 # 源码安装 定义路径 
 # source .bash_profile
+
+# 修改PSI
+export PS1="\[\033]2;\h:\u \w\007\033[32;1m\]\u@$ip\h \033[35;1m\t\033[0m \[\033[34;1m\]\w\[\033[0m\] \[\e[31;1m\] # \[\e
+[0m\]"
